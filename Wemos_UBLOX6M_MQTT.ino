55997cdfe2dd81855bcfdaaaaf0eeba8f768a4d6
Nazmi Febrian <nazmi.febrian@gmail.com>
2019-02-10 22:28:56 +0700

    - Delete double read GPS

--- Wemos_UBLOX6M_MQTT.ino
+++ Wemos_UBLOX6M_MQTT.ino
@@ -4,20 +4,21 @@
 #include <SoftwareSerial.h>
 
 #define wifi_ssid "SSID"
-#define wifi_password "PASSWD"
+#define wifi_password "PASS!"
 
-#define mqtt_server "IP"
-#define mqtt_user "username"
-#define mqtt_password "password"
+#define mqtt_server   "m16.cloudmqtt.com"
+#define mqtt_user     "user"
+#define mqtt_password "pass"
+#define mqtt_port     17708
 
-#define latitude_topic "latitude"
+#define gps_topic       "gpsData"
+#define latitude_topic  "latitude"
 #define longitude_topic "longitude"
 
 static const int RXPin = 4, TXPin = 5;
 static const uint32_t GPSBaud = 9600;
 
 float latitude , longitude;
-byte GPSData;
 
 long lastMsg = 0;   
 bool debug = true;
@@ -31,7 +32,7 @@ PubSubClient client(espClient);
 void setup() {
   Serial.begin(9600);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
+  client.setServer(mqtt_server, mqtt_port);
   ss.begin(GPSBaud);
 }
 
@@ -79,31 +80,31 @@ void loop() {
     reconnect();
   }
   client.loop();
-  
-  if (debug) {
-    while (ss.available() > 0) {
-//        GPSData = ss.read();
-//        Serial.write(GPSData);
-        
-      if (gps.encode(ss.read())) {
-        if (gps.location.isValid()) {
-          latitude = gps.location.lat();
-          longitude = gps.location.lng();
-        }
-      }    
-    }
-      
-    Serial.print("Latitude : ");
-    Serial.print(latitude);
-    Serial.print(" | Longitude : ");
-    Serial.println(longitude);     
-  }
 
+  while (ss.available() > 0) {
+    if (gps.encode(ss.read())) {
+      if (gps.location.isValid()) {
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+      }
+    }    
+  }
+    
   long now = millis();
-  // Send a message every second
-  if(now -  lastMsg > 1000 * 1){
+  // Send a message every 10 second
+  if(now -  lastMsg > 10000 * 1){
     lastMsg = now;
+
+    if (debug) {
+      Serial.print("Latitude : ");
+      Serial.print(latitude);
+      Serial.print(" | Longitude : ");
+      Serial.println(longitude);     
+    }
+
+    if (latitude != 0 && longitude != 0) {      
       client.publish(latitude_topic, String(latitude).c_str(), true);
       client.publish(longitude_topic, String(longitude).c_str(), true);
-  }  
+    }  
+  }
 }
